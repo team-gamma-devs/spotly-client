739bb141a8fed43a7f3de9405b62a04bcf306078
Gabriel Barn <gbrielbbarn@gmail.com>
2025-10-05 00:07:48 -0300

    feat: responsive header layout and dark theme

--- client/src/lib/components/main/Header.svelte
+++ client/src/lib/components/main/Header.svelte
@@ -10,26 +10,26 @@
         DropdownItem,
         DropdownHeader,
         DropdownGroup,
+        DarkMode,
     } from "flowbite-svelte";
     import AuthBox from "../AuthBox.svelte";
-    import Unauthorized from "../error/Unauthorized.svelte";
 </script>
 
-<Navbar class="bg-header-gradient py-0">
+<Navbar class="bg-header-gradient py-0 zalando-font">
     <NavBrand href="/">
         <enhanced:img
             src="$lib/assets/svgs/spotly-logo-color.svg"
-            class="me-3 w-6 h-6 sm:h-9 sm:w-9"
+            class="me-3 w-9 h-9 ml-2"
             alt="Spotly Logo"
         ></enhanced:img>
         <span
-            class="self-center text-xl text-white font-semibold whitespace-nowrap dark:text-foreground py-4"
+            class="self-center text-xl text-white font-semibold whitespace-nowrap dark:text-foreground py-4 hidden sm:inline"
             >SPOTLY</span
         >
     </NavBrand>
     <AuthBox>
         {#snippet authorizedContent()}
-            <NavUl class="text-white ">
+            <NavUl class="text-white">
                 <NavLi href="/dashboard" class="text-inherit hover:text-accent"
                     >Dashboard</NavLi
                 >
@@ -37,7 +37,7 @@
                     >Status</NavLi
                 >
             </NavUl>
-            <div class="flex items-center md:order-2 cursor-pointer">
+            <div class="flex items-center md:order-2 cursor-pointer mr-2 ml-auto sm:mr-2 sm:ml-2">
                 <Avatar
                     id="avatar-menu"
                     src="images/abstract-user-flat-4.svg"
@@ -45,6 +45,7 @@
                 />
                 <NavHamburger class="cursor-pointer" />
             </div>
+            <DarkMode class="mx-2 md:mr-4 md:ml-auto p-2 cursor-pointer text-white hover:text-black dark:hover:text-white"/>
             <Dropdown placement="bottom" triggeredBy="#avatar-menu">
                 <DropdownHeader>
                     <span class="block text-sm">Pepe Pelotas</span>
@@ -60,6 +61,7 @@
             </Dropdown>
         {/snippet}
         {#snippet unauthorizedContent()}
+            <DarkMode />
             <p class="text-white font-bold">You must login</p>
         {/snippet}
     </AuthBox>
--- client/src/lib/stores/auth.ts
+++ client/src/lib/stores/auth.ts
@@ -1,3 +1,3 @@
 import { writable } from "svelte/store"
 
-export const isAuth = writable(false) 
+export const isAuth = writable(true) 
