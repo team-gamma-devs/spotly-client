ed462ce3370ed1e664be4671301844b3f2c887d4
Gabriel Barn <gbrielbbarn@gmail.com>
2025-10-27 00:24:58 -0300

    fix: supabase envs

--- client/src/lib/supabaseClient.ts
+++ client/src/lib/supabaseClient.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
-// import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY } from "$env/static/public";
-import { SUPABASE_URL, SUPABASE_PUBLISHABLE_DEFAULT_KEY } from "$env/static/private";
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY } from "$env/static/public";
 
-const supabaseUrl = SUPABASE_URL;
-const supabaseKey = SUPABASE_PUBLISHABLE_DEFAULT_KEY;
+
+const supabaseUrl = PUBLIC_SUPABASE_URL;
+const supabaseKey = PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
