86c2de8af257aacbde7a4bce328a06f349727d88
Gabriel Barn <gbrielbbarn@gmail.com>
2025-10-28 18:54:49 -0300

    feat: add dynamic tags filtering from backend

--- client/src/routes/app/manager/+page.svelte
+++ client/src/routes/app/manager/+page.svelte
@@ -1,13 +1,14 @@
-<script>
-	import Sidebar from '$lib/components/manager/ManagerSidebar.svelte';
-	// import { page } from "$app/state";
-	// page.data.title = ""
+<script lang="ts">
+    import Sidebar from '$lib/components/manager/ManagerSidebar.svelte';
+    import type { PageData } from './$types';
+
+    let { data }: { data: PageData } = $props();
 </script>
 
 <section id="dashboard-wrapper" class="w-full">
-	<!-- For simplicity sake, send the sidebar (components/graduate/ManagerSidebar.svelte) data fetched on this page's server load as props,
+		<!-- For simplicity sake, send the sidebar (components/graduate/ManagerSidebar.svelte) data fetched on this page's server load as props,
 	 		the layout for this page is handled inside the sidebar because it has a component to render stuff there that is easier to control,
 			otherwise you'll gonna have to mess with this page responsiveness it's easier to just let flowbite handle responsiveness within
 			the sidebar component. -->
-	<Sidebar />
+    <Sidebar availableTags={data.availableTags} tagsError={data.tagsError} />
 </section>
