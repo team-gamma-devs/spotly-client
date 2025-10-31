e984138e18fb447902161e714126ef32bd4661a2
Gabriel Barn <gbrielbbarn@gmail.com>
2025-10-31 19:50:50 -0300

    refactor: clean up mockUserState formatting

--- client/src/lib/mocks/mockUserState.ts
+++ client/src/lib/mocks/mockUserState.ts
@@ -1,77 +1,45 @@
-export interface MongoObjectId {
-  $oid: string;
-}
-export interface MongoDate {
-  $date: string;
-}
-export type UserRole = 'graduate' | 'manager';
-export type EnglishLevel = 'basic' | 'intermediate' | 'advanced';
-export interface UserCVInfo {
-  githubInfo: string;
-  personalCvUrl: string;
-  personalCvPath: string;
-  linkedinUrl: string;
-  skills: string[];
-  englishLevel: EnglishLevel;
-  worksInIt: boolean;
-  lastUpdate: MongoDate;
-}
-export interface UserState {
-  id: MongoObjectId;
-  firstName: string;
-  lastName: string;
-  email: string;
-  avatarUrl: string;
-  avatarPath: string;
-  role: UserRole;
-  createdAt: MongoDate;
-  updatedAt: MongoDate;
-  cvInfo: UserCVInfo;
-  cohort: number;
-}
+import type { UserState } from "$lib/types/userFull";
+
 export const mockUserState: UserState = {
-  id: { $oid: '68fd528c007c411b4191e836' },
-  firstName: 'Federico',
-  lastName: 'Paganini',
-  email: 'perfedefer@gmail.com',
-  avatarUrl:'',
-  avatarPath: '',
-  role: 'graduate',
-  createdAt: { $date: '2025-10-25T19:43:24.025Z' },
-  updatedAt: { $date: '2025-10-25T19:43:24.025Z' },
-  cvInfo: {
-    githubInfo: 'https://github.com/federico-paganini',
-    personalCvUrl:
-'',
-    personalCvPath: '',
-    linkedinUrl:
-'',
-    skills: [
-'Docker',
-'React',
-'Git',
-'SQL',
-'Flask',
-'Nginx',
-'MongoDB',
-'AWS',
-'JavaScript',
-'TypeScript',
-'Docker Compose',
-'C',
-'Bootstrap',
-'Node.js',
-'REST',
-'CSS',
-'Unittest',
-'Python',
-'FastAPI',
-'HTML',
-'GitHub',
-],
-    englishLevel: 'advanced',
-    worksInIt: true,
-    lastUpdate: { $date: '2025-10-25T19:43:23.457Z' },
-},
-  cohort: 26,
+	id: '68fd528c007c411b4191e836',
+	firstName: 'Federico',
+	lastName: 'Paganini',
+	email: 'perfedefer@gmail.com',
+	avatarUrl: 'https://a.storyblok.com/f/191576/1176x882/f95162c213/profile_picture_hero_before.webp',
+	cohort: 26,
+	githubUsername: 'https://github.com/federico-paganini',
+	cvInfo: {
+		personalCvUrl: '',
+		linkedinUrl: '',
+		skills: [
+			'Docker',
+			'React',
+			'Git',
+			'SQL',
+			'Flask',
+			'Nginx',
+			'MongoDB',
+			'AWS',
+			'JavaScript',
+			'TypeScript',
+			'Docker Compose',
+			'C',
+			'Bootstrap',
+			'Node.js',
+			'REST',
+			'CSS',
+			'Unittest',
+			'Python',
+			'FastAPI',
+			'HTML',
+			'GitHub',
+		],
+		englishLevel: 'Advanced',
+		worksInIt: true,
+		lastUpdate: '2025-10-25T19:43:23.457Z',
+	},
+	tutorsFeedback: [],
+	role: 'graduate',
+	createdAt: '2025-10-25T19:43:24.025Z',
+	updatedAt: '2025-10-25T19:43:24.025Z',
 };
